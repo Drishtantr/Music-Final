27b0f02fea5c5c241242520d1fc0e8d43ef86d76
drishtantr <drishtantregmi777@gmail.com>
2020-08-04 21:33:55 +0545

    first commit

---
+++ static/js/genres.js
@@ -0,0 +1,14 @@
+var GENRE_TO_COLOR = new Map([
+    ['blues', '#799FCB'],
+    ['classical', '#FFB447'],
+    ['country', '#FCE2C2'],
+    ['disco', '#E18AAA'],
+    ['hiphop', '#957DAD'],
+    ['jazz', '#Jazz'],
+    ['metal', '#999966'],
+    ['pop', '#F9665E'],
+    ['reggae', '#ADDFAC'],
+    ['rock', '#ACACAC']
+]);
+
+var GENRES = Array.from(GENRE_TO_COLOR.keys());
